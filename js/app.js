2e389a6b0fe862903ab61d68f3da0102e4b9aee4
VinzGh1 <88178376+VinzGh1@users.noreply.github.com>
2022-09-29 18:12:43 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -8,6 +8,27 @@ function init() {
    </div>
    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;700&display=swap" rel="stylesheet">
   <link href='https://fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
+  <style>
+  .copyright {
+        bottom: 0; 
+        font-size:14px; 
+        font-family:'Lato', sans-serif; 
+        font-weight: 300; 
+        letter-spacing: normal; 
+        line-height: 1.3; 
+        text-transform: capitalize; 
+        color:white;
+      }
+      
+      .copyright-links {
+      text-decoration: none;
+      }
+      
+      .copyright-links:hover {
+        text-decoration: underline;
+        color: white;
+      }
+  </style>
 </header>
 <div>
 <div id="content" style="padding-top: ${UI.header_padding}px;${UI.fixed_footer ?' padding-bottom: clamp(170px, 100%, 300px);': ''}">
@@ -31,6 +52,7 @@ function init() {
 <br>
 <footer class="footer mt-auto py-3 text-muted ${UI.footer_style_class}" style="${UI.fixed_footer ?'position: fixed; ': ''}left: 0; bottom: 0; width: 100%; text-align: center; color: white; z-index: 9999;${UI.hide_footer ? ' display:none;': ' display:block;'}"> <div class="container" style="width: auto; padding: 0 10px;"> ${UI.credit ? '<h1 style="font-family: Montserrat; font-weight: 700; font-size:18px; color: white; letter-spacing: normal; line-height: 1.3;">SUPPORT</h1>' : ''}<a href="https://t.me/synerize" target="_blank"><img alt="Telegram" src="https://img.shields.io/badge/Telegram-2CA5E0?style=for-the-badge&logo=telegram&logoColor=white"/></a>
 <a href="https://www.facebook.com/kazu.miteku.1" target="_blank"><img alt="Facebook" src="https://img.shields.io/badge/Facebook-1877F2?style=for-the-badge&logo=facebook&logoColor=white"/></a>
+<a href="https://saweria.co/Ryoo1" target="_blank"><img alt="Donation" src="https://img.shields.io/badge/🎁DONASI-e62929?style=for-the-badge&logo=Trakteer&logoColor=white"/></a>
 <p style="margin-top: 10px; 
         font-size: 15px; 
         font-family:'Montserrat', sans-serif; 
@@ -38,7 +60,7 @@ function init() {
         color: #ffffff; 
         line-height:1.2; 
         letter-spacing: normal;">Jumlah File: 22939</p>
-  <p>© ${UI.copyright_year} - <a href=" ${UI.company_link}" target="_blank"> ${UI.company_name}</a></p> </div> </footer>
+  <p class="copyright">© ${UI.copyright_year} - <a href=" ${UI.company_link}" target="_blank"> ${UI.company_name}</a></p> </div> </footer>
   `;
     $('body').html(html);
 }
